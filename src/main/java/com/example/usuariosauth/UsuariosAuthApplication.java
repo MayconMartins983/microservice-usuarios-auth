e867dbf42cb2776d1d0d48a466cf336746bc4076
Raul Correia <raulvinicius245@gmail.com>
2023-07-03 16:12:09 -0300

    feat: 1.0 - Iniciado projeto do microservico de usuarios.

---
+++ src/main/java/com/example/usuariosauth/UsuariosAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.usuariosauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsuariosAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usuariosauth/UsuariosAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usuariosauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
